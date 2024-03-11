9317f072f1ab68bd7cdf050562e942bc2d1a867c
Déborah <deborahfagund@gmail.com>
2024-03-11 14:06:41 -0300

    detalhes 2

--- script.js
+++ script.js
@@ -114,7 +114,7 @@ const editDelete = (event) => {
             editClient(index)
         } else {
             const client = readClient()[index]
-            const response = confirm(`Deseja realmente excluir o cliente ${client.nome}`)
+            const response = confirm(`Deseja realmente excluir o pet ${client.nome}`)
             if (response) {
                 deleteClient(index)
                 updateTable()
